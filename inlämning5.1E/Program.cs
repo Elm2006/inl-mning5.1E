b0943569fc15e8e848121a8edd716aae80990e69
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-22 09:51:16 +0100

    Add project files.

---
+++ inlämning5.1E/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace inlämning5.__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("Skriv ett namn");
+                namn[i] = Console.ReadLine();
+            }
+            for (int i = 0;i < 5; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
